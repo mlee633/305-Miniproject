8c04a43d3ee0cb9f0cf220fd95d914fc8abf9fa2
sela847 <sela847@aucklanduni.ac.nz>
2023-05-04 18:55:41 +1200

    A bit broken code when implementing pipe system
    -Need to fix pipe code which hopefully fix the mouse action

--- bouncy_ball.vhd
+++ bouncy_ball.vhd
@@ -14,35 +14,39 @@ END bouncy_ball;
 
 architecture behavior of bouncy_ball is
 
-SIGNAL ball_on					: std_logic;
-SIGNAL size 					: std_logic_vector(9 DOWNTO 0);  
+SIGNAL ball_on, pipe1_on, pipe2_on				: std_logic;
+SIGNAL size, pipeSize1, pipeSize2, pipeWidth,pipe_x_pos,pipe1_y_pos,pipe2_y_pos 					: std_logic_vector(9 DOWNTO 0);  
 SIGNAL ball_y_pos				: std_logic_vector(9 DOWNTO 0) := CONV_STD_LOGIC_VECTOR(200, 10);
-SiGNAL ball_x_pos				: std_logic_vector(10 DOWNTO 0);
+SiGNAL ball_x_pos				: std_logic_vector(9 DOWNTO 0);
 SIGNAL prev_clicked        : std_logic := '0';
 SIGNAL started					: std_logic := '0';
 SIGNAL finished				: std_logic := '1';
 BEGIN           
-
+--size = ball size. 
 size <= CONV_STD_LOGIC_VECTOR(12,10);
-pipeSize1 <= CONV_STD_LOGIC_VECTOR(180, 10);
-pipeSize2 <= CONV_STD_LOGIC_VECTOR(300, 10);
+pipeSize1 <= CONV_STD_LOGIC_VECTOR(90, 10);
+pipeSize2 <= CONV_STD_LOGIC_VECTOR(90, 10);
 pipeWidth <= CONV_STD_LOGIC_VECTOR(24, 10);
 
 -- ball_x_pos and ball_y_pos show the (x,y) for the centre of ball
-ball_x_pos <= CONV_STD_LOGIC_VECTOR(200,11);
+ball_x_pos <= CONV_STD_LOGIC_VECTOR(200,10);
+
+pipe1_y_pos <= CONV_STD_LOGIC_VECTOR(90,10);
+pipe2_y_pos <= CONV_STD_LOGIC_VECTOR(400,10);
+pipe_x_pos <= CONV_STD_LOGIC_VECTOR(500,10);
 
 
---pipe1_on <= '1' when ((pixel_row - pipeWidth)*(pixel_column - pipeSize1) <= pipeWidth * pipeSize1 else '0';
-
+pipe1_on <= '1' when (('0' & pipe_x_pos <= '0' & pixel_column + pipeWidth) and ('0' & pixel_column<= '0' & pipe_x_pos + pipeWidth) and ('0' & pipe1_y_pos <= pixel_row + pipesize1) and ('0' & pixel_row <= pipe1_y_pos + pipesize1)) else '0' ;
+pipe2_on <= '1' when (('0' & pipe_x_pos <= '0' & pixel_column + pipeWidth) and ('0' & pixel_column<= '0' & pipe_x_pos + pipeWidth) and ('0' & pipe2_y_pos <= pixel_row + pipesize2) and ('0' & pixel_row <= pipe2_y_pos + pipesize2)) else '0' ;
 -- <=.....this a store or less than equal.  
 ball_on <= '1' when ((pixel_column - ball_x_pos) * (pixel_column - ball_x_pos) + (pixel_row - ball_y_pos) * (pixel_row - ball_y_pos) <= size * size) else '0';
 
 
 -- Colours for pixel data on video signal
 -- Changing the background and ball colour by pushbuttons
-Red <=  pb1; --won't work for some reason
-Green <= (not pb2) and (not ball_on);
-Blue <=  not ball_on;
+Red <=  ball_on; --won't work for some reason
+Green <= pipe1_on or pipe2_on;
+Blue <=  (not ball_on) and (not pipe1_on) and (not pipe2_on);
 
 --Aight don't do Key0. Somehow now resets whole board -__-
 CLICK: process(pb2)
@@ -56,7 +60,7 @@ begin
 			numOfPush := 0;
 		end if;
 	end if;
-end process;
+end process CLICK;
 
 --leftclick, ball_y_motion,ball_y_pos,
 BOUNCE_BALL: process(vert_sync)
@@ -68,7 +72,7 @@ begin
 				--start at current value of speed. from beginning should be 0.....hopefully
 				ball_y_motion := ball_y_motion;
 				--Stuck on the bottom. Change the parameter for conversion to a 10 bit std logic vector to find sweet spot of the bottom of the moving object. 
-				if (ball_y_pos >= CONV_STD_LOGIC_VECTOR(450,10) + size) then
+				if (ball_y_pos >= CONV_STD_LOGIC_VECTOR(450,10) + size) or (ball_y_pos <= CONV_STD_LOGIC_VECTOR(10,10) - size) then
 				--stays at current position
 					ball_y_motion := CONV_STD_LOGIC_VECTOR(0, 10); --resets to 0 speed
 				else
