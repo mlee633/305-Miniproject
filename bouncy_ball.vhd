654e917dc4b943468b8e7cf276e9fe7f724e4a13
Shaaran Elango <sela847@UoA.auckland.ac.nz>
2023-05-09 15:06:42 +1200

    made a working pipe gap

--- bouncy_ball.vhd
+++ bouncy_ball.vhd
@@ -15,7 +15,7 @@ END bouncy_ball;
 architecture behavior of bouncy_ball is
 
 SIGNAL ball_on, pipe1_on, pipe2_on				: std_logic;
-SIGNAL size, pipeSize1, pipeSize2, pipeWidth,pipe_x_pos,pipe1_y_pos,pipe2_y_pos 					: std_logic_vector(9 DOWNTO 0);  
+SIGNAL size, pipeSize1,gapSize, pipeSize2,gap_x_pos, gap_y_pos, pipeWidth,pipe_x_pos,pipe1_y_pos,pipe2_y_pos 					: std_logic_vector(9 DOWNTO 0);  
 SIGNAL ball_y_pos				: std_logic_vector(9 DOWNTO 0) := CONV_STD_LOGIC_VECTOR(200, 10);
 SiGNAL ball_x_pos				: std_logic_vector(9 DOWNTO 0);
 SIGNAL prev_clicked        : std_logic := '0';
@@ -25,19 +25,26 @@ BEGIN
 --size = ball size. 
 size <= CONV_STD_LOGIC_VECTOR(12,10);
 pipeSize1 <= CONV_STD_LOGIC_VECTOR(90, 10);
-pipeSize2 <= CONV_STD_LOGIC_VECTOR(90, 10);
+--pipeSize2 <= CONV_STD_LOGIC_VECTOR(140, 10);
 pipeWidth <= CONV_STD_LOGIC_VECTOR(24, 10);
 
+gapSize <= CONV_STD_LOGIC_VECTOR(120,10);
 -- ball_x_pos and ball_y_pos show the (x,y) for the centre of ball
 ball_x_pos <= CONV_STD_LOGIC_VECTOR(200,10);
 
+gap_y_pos <= CONV_STD_LOGIC_VECTOR(239,10);
+gap_x_pos <= CONV_STD_LOGIC_VECTOR(250,10);
 pipe1_y_pos <= CONV_STD_LOGIC_VECTOR(90,10);
-pipe2_y_pos <= CONV_STD_LOGIC_VECTOR(400,10);
+--pipe2_y_pos <= CONV_STD_LOGIC_VECTOR(300,10);
 pipe_x_pos <= CONV_STD_LOGIC_VECTOR(500,10);
 
 
-pipe1_on <= '1' when (('0' & pipe_x_pos <= '0' & pixel_column + pipeWidth) and ('0' & pixel_column<= '0' & pipe_x_pos + pipeWidth) and ('0' & pipe1_y_pos <= pixel_row + pipesize1) and ('0' & pixel_row <= pipe1_y_pos + pipesize1)) else '0' ;
-pipe2_on <= '1' when (('0' & pipe_x_pos <= '0' & pixel_column + pipeWidth) and ('0' & pixel_column<= '0' & pipe_x_pos + pipeWidth) and ('0' & pipe2_y_pos <= pixel_row + pipesize2) and ('0' & pixel_row <= pipe2_y_pos + pipesize2)) else '0' ;
+-- pipe1_on <= '1' when (('0' & gap_x_pos <= '0' & pixel_column + pipeWidth) and 
+-- 							('0' & pixel_column<= '0' & gap_x_pos + pipeWidth) and 
+-- 							('0' & gap_y_pos + gapSize <= pixel_row ) and 
+-- 							('0' & pixel_row + gapSize <= gap_y_pos )) else '0' ;
+pipe1_on <= '1' when ((gap_x_pos - pipeWidth <= pixel_column) and (pixel_column <= gap_x_pos + pipeWidth) and ((gap_y_pos + gapSize <= pixel_row) or (pixel_row<= gap_y_pos-gapSize))) else '0';
+--pipe2_on <= '1' when (('0' & pipe_x_pos <= '0' & pixel_column + pipeWidth) and ('0' & pixel_column<= '0' & pipe_x_pos + pipeWidth) and ('0' & pipe2_y_pos <= pixel_row + pipesize2) and ('0' & pixel_row <= pipe2_y_pos + pipesize2)) else '0' ;
 -- <=.....this a store or less than equal.  
 ball_on <= '1' when ((pixel_column - ball_x_pos) * (pixel_column - ball_x_pos) + (pixel_row - ball_y_pos) * (pixel_row - ball_y_pos) <= size * size) else '0';
 
@@ -45,7 +52,8 @@ ball_on <= '1' when ((pixel_column - ball_x_pos) * (pixel_column - ball_x_pos) +
 -- Colours for pixel data on video signal
 -- Changing the background and ball colour by pushbuttons
 Red <=  ball_on; --won't work for some reason
-Green <= pipe1_on or pipe2_on;
+--Green <= pipe1_on or pipe2_on;]
+Green <= pipe1_on;
 Blue <=  (not ball_on) and (not pipe1_on) and (not pipe2_on);
 
 --Aight don't do Key0. Somehow now resets whole board -__-
