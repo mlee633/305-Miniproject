0f09c596102922163dcb51c63a6c76e75a494434
mlee633 <mlee633@aucklanduni.ac.nz>
2023-05-22 15:23:29 +1200

    Added collision

--- bouncy_ball.vhd
+++ bouncy_ball.vhd
@@ -11,26 +11,30 @@ ENTITY bouncy_ball IS
           pixel_row, pixel_column	: IN std_logic_vector(9 DOWNTO 0);
 		  oneSeg_out : out std_logic_vector(6 downto 0);
 		  tenSeg_out : out std_logic_vector(6 downto 0);
-		  red, green, blue 			: OUT std_logic);		
+		  red, green, blue,collide 			: OUT std_logic);		
 END bouncy_ball;
 
 architecture behavior of bouncy_ball is
 
-SIGNAL rom_mux_output, ball_on, pipe_test, pipe_red, pipe_blue, pipe_green, t_Clk,reset,enable1,enable2		: std_logic;
-SIGNAL size, pipeSize1,gapSize, pipeSize2,gap_x_pos, gap_y_pos, pipeWidth,pipe_x_pos,pipe1_y_pos,pipe2_y_pos 					: std_logic_vector(9 DOWNTO 0);  
+SIGNAL rom_mux_output, ball_on, pipe_on, pipe_red, pipe_blue, pipe_green,green1, t_Clk,reset,enable1,enable2,dead,t_collide		: std_logic;
+SIGNAL size, pipeSize1,gapSize, pipeSize2,gap_x_pos, gap_y_pos, pipeWidth,pipe_x_pos 					: std_logic_vector(9 DOWNTO 0);  
 SIGNAL ball_y_pos				: std_logic_vector(9 DOWNTO 0) := CONV_STD_LOGIC_VECTOR(200, 10);
-SiGNAL ball_x_pos				: std_logic_vector(9 DOWNTO 0);
+SiGNAL ball_x_pos			: std_logic_vector(9 DOWNTO 0);
 SIGNAL prev_clicked        : std_logic := '0';
 SIGNAL started					: std_logic := '0';
 SIGNAL finished				: std_logic := '1';
 SIGNAL ones,tens				: std_logic_vector(3 downto 0);
 signal oneSeg,tenSeg			: std_logic_vector(6 downto 0);
 SIGNAL character_address	: std_logic_vector(5 downto 0);
+-- component collision is
+-- 	port (vert_sync, ball_on, pipe_on,started : in std_logic;
+-- 		collide : out std_logic);
+-- 	end component;
 component pipe is
 	port (
-        clk,vert_sync   : in std_logic;
+        clk,vert_sync,enable   : in std_logic;
         pixel_row,pixel_column : in std_logic_vector(9 DOWNTO 0);
-		  gap_x_pos_out : out std_logic_vector(9 downto 0);
+		  pipeWidth_out,gapSize_out,gap_x_pos_out,gap_y_pos_out : out std_logic_vector(9 downto 0);
         red,green,blue, pipe_test : out std_logic
     );
 end component;
@@ -42,7 +46,6 @@ component text_setter is
         clock                               :     IN STD_LOGIC
     );
 end component;
-
 component char_rom IS
 	PORT
 	(
@@ -52,8 +55,6 @@ component char_rom IS
 		rom_mux_output		:	OUT STD_LOGIC
 	);
 END component;
-
-
   component BCD is
     port(Clk, Direction, Init, Enable: in std_logic;
         Q_Out: out std_logic_vector(3 downto 0));
@@ -66,7 +67,7 @@ END component;
 BEGIN 
 
 
--- Creating the text on the screen using char_rom and a text setter
+	 
 textSetter : text_setter port map(
 		      character_address	=> character_address,
 		pixel_row => pixel_row(9 downto 4), pixel_col => pixel_column(9 downto 4),
@@ -79,20 +80,15 @@ charRom: char_rom port map(
 		clock	=> clk,
 		rom_mux_output => rom_mux_output
     );
-	 
- -- Adding the pipe
- 
-pipeone: pipe port map (clk => clk, vert_sync => vert_sync, pixel_row => pixel_row, 
-								pixel_column => pixel_column, gap_x_pos_out => gap_x_pos ,red => pipe_red, green => pipe_green, blue => pipe_blue, pipe_test => pipe_test);           
 
--- BCD for counting the score
+--collision_detect: collision port map (vert_sync => vert_sync, ball_on => ball_on, pipe_on => pipe_on,started => started,collide => t_collide);
+pipeone: pipe port map (clk => clk, vert_sync => vert_sync,enable => (not dead) and started, pixel_row => pixel_row, 
+								pixel_column => pixel_column,pipeWidth_out => pipeWidth,gapSize_out => gapSize, gap_x_pos_out => gap_x_pos,gap_y_pos_out => gap_y_pos ,red => pipe_red, green => pipe_green, blue => pipe_blue, pipe_test => pipe_on);           
+
 onesBCD: BCD port map (Clk => t_clk, Direction => '1', Init => reset, Enable => enable1, Q_out => ones); 
 tensBCD: BCD port map (Clk => t_clk, Direction => '1', Init => reset, Enable => enable2, Q_out => tens);
-
--- 7 segment for the score
 one_segment: BCD_to_7Seg port map (BCD_digit => ones, SevenSeg_out => oneSeg);
 ten_segment: BCD_to_7Seg port map (BCD_digit => tens, SevenSeg_out => tenSeg);
-
 --size = ball size. 
 size <= CONV_STD_LOGIC_VECTOR(12,10);
 
@@ -103,17 +99,19 @@ ball_x_pos <= CONV_STD_LOGIC_VECTOR(200,10);
   
 ball_on <= '1' when ((pixel_column - ball_x_pos) * (pixel_column - ball_x_pos) + (pixel_row - ball_y_pos) * (pixel_row - ball_y_pos) <= size * size) else '0';
 
-
 -- Colours for pixel data on video signal
 -- Changing the background and ball colour by pushbuttons
 Red <=  (ball_on) or (pipe_red) or (rom_mux_output); 
-Green <= ((pipe_green) and (not ball_on)) or (rom_mux_output);
+
+Green1 <= pipe_green or (rom_mux_output);
+green <= green1;
 Blue <=  ((not ball_on) and (pipe_blue)) or (rom_mux_output);
 
 --Aight don't do Key0. Somehow now resets whole board -__-
 CLICK: process(pb2)
 variable numOfpush : integer := 0;
 begin
+	
 	if rising_edge(pb2) then
 		numOfpush := numOfpush + 1;
 		if (numOfPush = 2) then
@@ -122,45 +120,55 @@ begin
 			reset <= '1';
 		else
 			started <= '1';
-			
 			reset <='0';
 		end if;
 	end if;
 end process CLICK;
 
+
+
+
+
 --leftclick, ball_y_motion,ball_y_pos,
 BOUNCE_BALL: process(vert_sync)
 	variable ball_y_motion: std_logic_vector(9 downto 0) := CONV_STD_LOGIC_VECTOR(0, 10);
+
 begin
+		
 		if (rising_edge(vert_sync)) then
 		--When button has been  pressed
 			if (started = '1')then
-				--start at current value of speed. from beginning should be 0.....hopefully
-				
+			--start at current value of speed. from beginning should be 0.....hopefully
 				ball_y_motion := ball_y_motion;
+				
 				--Stuck on the bottom. Change the parameter for conversion to a 10 bit std logic vector to find sweet spot of the bottom of the moving object. 
-				if (ball_y_pos >= CONV_STD_LOGIC_VECTOR(450,10) + size) or (ball_y_pos <= CONV_STD_LOGIC_VECTOR(10,10) - size) then
+				if ((dead = '1') or ball_y_pos >= CONV_STD_LOGIC_VECTOR(450,10) + size) or (ball_y_pos <= CONV_STD_LOGIC_VECTOR(10,10) - size) or (((gap_x_pos - pipeWidth <= ball_x_pos) and (ball_x_pos<= gap_x_pos + pipeWidth) and  not (( gap_y_pos - gapSize<=ball_y_pos) and (ball_y_pos <=gap_y_pos + gapSize)))) then
 				--stays at current position
+					dead <= '1';
+					
 					ball_y_motion := CONV_STD_LOGIC_VECTOR(0, 10); --resets to 0 speed
+					
 					enable1 <= '0';
 				else
-					enable1 <= '1';
-					
+					dead <= '0';
 					if (ones = "1001" and enable1 = '1') then
-						enable2 <= '1';
+							enable2 <= '1';
 					else
-						enable2 <= '0';
+							enable2 <= '0';
 					end if;
 					prev_clicked <= leftclick;
 					if ((leftclick /= '0') and (prev_clicked = '0') ) then
 						--Set intial up speed to be 10
+						enable1 <= '1';
 						ball_y_motion := -CONV_STD_LOGIC_VECTOR(10,10);
 						if (ball_y_motion <= -CONV_STD_LOGIC_VECTOR(3,10)) then
+							
 							ball_y_motion := ball_y_motion + CONV_STD_LOGIC_VECTOR(1,10);
 						else
 							ball_y_motion := ball_y_motion;
 						end if;
 					else
+						enable1 <= '1';
 						if (ball_y_motion <= CONV_STD_LOGIC_VECTOR(10,10)) then
 							ball_y_motion :=  ball_y_motion + CONV_STD_LOGIC_VECTOR(1,10);
 						else
@@ -176,13 +184,22 @@ begin
 						t_clk <= '0';
 					end if;
 				end if;
+			
 			else
+			--Game not started
+				dead <= '0';
 				--reset to starting position
 				ball_y_pos <= CONV_STD_LOGIC_VECTOR(200,10);
+				t_Clk <= '1';
 			end if;
 		end if;
-		
+
+		--end if;
+
 end process BOUNCE_BALL;
+-- else '0' when started = '1';
+--t_collide <= '1' when ((ball_on = '1') and (pipe_on = '1')) else '0';
+collide <= t_collide;
 oneSeg_out <= oneSeg;
 tenSeg_out <= tenSeg;
 
--- pipe.vhd
+++ pipe.vhd
@@ -1,31 +1,45 @@
 LIBRARY IEEE;
 USE IEEE.STD_LOGIC_1164.all;
-USE  IEEE.STD_LOGIC_ARITH.all;
+--use IEEE.std_logic_arith.all;
 USE  IEEE.STD_LOGIC_UNSIGNED.all;
+--use ieee.std_logic_signed.all;
+use ieee.numeric_std.all;
+use ieee.math_real.all;
 
 entity pipe is
     port (
-        clk,vert_sync : in std_logic;
+        clk,vert_sync,enable : in std_logic;
         pixel_row,pixel_column : in std_logic_vector(9 DOWNTO 0);
-		  gap_x_pos_out : out std_logic_vector(9 downto 0);
+        pipeWidth_out,gapSize_out, gap_x_pos_out ,gap_y_pos_out: out std_logic_vector(9 downto 0);
         red,green,blue, pipe_test : out std_logic
     );
 end entity;
 
 architecture behaviour of pipe is
+component LFSR is
+        port (
+            clk ,reset  : in std_logic;
+
+            outLFSR: out integer
+            
+        );
+    end component;
     signal pipe1_on : std_logic;
     signal size,pipe_y_pos, pipeSize1, gapSize : std_logic_vector(9 DOWNTO 0);
-	 signal gap_x_pos : std_logic_vector(9 downto 0) := CONV_STD_LOGIC_VECTOR(639,10);
-	 Signal gap_y_pos : std_logic_vector(9 downto 0) := CONV_STD_LOGIC_VECTOR(239,10);
-	 signal pipeWidth: std_logic_vector(9 downto 0) :=  CONV_STD_LOGIC_VECTOR(48, 10);
+	signal gap_x_pos : std_logic_vector(9 downto 0) ;--:= std_logic_vector(to_unsigned(639,10));
+	Signal gap_y_pos : std_logic_vector(9 downto 0) ;--:= std_logic_vector(to_unsigned(239,10));
+	signal pipeWidth: std_logic_vector(9 downto 0) :=  std_logic_vector(to_unsigned(48, 10));
+    signal tempHeight: integer;
+    signal reset: std_logic;
+    
     --signal pipe_x_pos : std_logic_vector(10 DOWNTO 0);
 begin
-    --pipeSize2 <= CONV_STD_LOGIC_VECTOR(140, 10);
-
-    gapSize <= CONV_STD_LOGIC_VECTOR(55,10);
-    --gap_y_pos <= CONV_STD_LOGIC_VECTOR(239,10);
-	 --gap_x_pos <= CONV_STD_LOGIC_VECTOR(250,10);
-    --pipe2_y_pos <= CONV_STD_LOGIC_VECTOR(300,10);
+    --pipeSize2 <= to_unsigned(140, 10);
+    randomHeight: LFSR port map (Clk   => clk, Reset => reset, outLFSR => tempHeight);
+    gapSize <= std_logic_vector(to_unsigned(55,10));
+    --gap_y_pos <= to_unsigned(239,10);
+	 --gap_x_pos <= to_unsigned(250,10);
+    --pipe2_y_pos <= to_unsigned(300,10);
     pipe1_on <= '1' when ((pixel_column > gap_x_pos - pipeWidth) and 
 								  (pixel_column < gap_x_pos) and 
 								 ((gap_y_pos + gapSize <= pixel_row) or (pixel_row<= gap_y_pos-gapSize)))
@@ -36,19 +50,33 @@ begin
 	 green <= pipe1_on;
 	 
 MOVE_PIPE: process (vert_sync)
+variable v_height : integer;
 begin
 
     if (rising_edge(vert_sync)) then
-        if (gap_x_pos < CONV_STD_LOGIC_VECTOR(1,10)) then
-            gap_x_pos <= CONV_STD_LOGIC_VECTOR(687,10);
-				pipeWidth <= CONV_STD_LOGIC_VECTOR(48,10);
-		  elsif (gap_x_pos - pipeWidth <= CONV_STD_LOGIC_VECTOR(1,10)) then
-				pipeWidth <= pipeWidth - CONV_STD_LOGIC_VECTOR(1,10);
-				gap_x_pos <= gap_x_pos - CONV_STD_LOGIC_VECTOR(1,10);
+		 if enable = '1' then
+        if (gap_x_pos < std_logic_vector(to_unsigned(1,10))) then
+            gap_x_pos <= std_logic_vector(to_unsigned(687,10));
+			pipeWidth <= std_logic_vector(to_unsigned(48,10));
+			
+			v_height := (375 * (tempHeight)); --+ 44;
+			v_height := (v_height/255) + 50;
+            gap_y_pos <= std_logic_vector(to_unsigned(v_height,10));
+		  elsif (gap_x_pos - pipeWidth <= std_logic_vector(to_unsigned(1,10))) then
+				pipeWidth <= pipeWidth - std_logic_vector(to_unsigned(1,10));
+				gap_x_pos <= gap_x_pos - std_logic_vector(to_unsigned(1,10));
         else
-            gap_x_pos <= gap_x_pos - CONV_STD_LOGIC_VECTOR(1,10);
+            gap_x_pos <= gap_x_pos - std_logic_vector(to_unsigned(1,10));
         end if;
+		 else
+			gap_x_pos <= std_logic_vector(to_unsigned(0,10));
+			gap_y_pos <= std_logic_vector(to_unsigned(0,10));
+		 end if;
+		  
     end if;
 end process;
+pipeWidth_out <= pipeWidth;
 gap_x_pos_out <= gap_x_pos;
+gap_y_pos_out <= gap_y_pos;
+gapSize_out <= pipeWidth;
 end architecture behaviour;
